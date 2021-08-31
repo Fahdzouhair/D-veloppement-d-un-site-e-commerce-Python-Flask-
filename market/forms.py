58649445e130ae2d1f6523909aee8b9dd0837c62
Fahd_ZOUHAIR <fahdzouhair47@gmail.com>
2021-08-31 18:28:57 +0100

    Item Purchasing 1

--- market/forms.py
+++ market/forms.py
@@ -29,3 +29,10 @@ class LoginForm(FlaskForm):
     username = StringField(label='User name: ', validators=[ DataRequired()])
     password = PasswordField(label='Password: ', validators=[ DataRequired()])
     submit = SubmitField(label='Sign in')
+
+
+class PurchaseItemForm(FlaskForm):
+    submit = SubmitField(label='Purchase item')
+
+class SellItemForm(FlaskForm):
+    submit = SubmitField(label='Sell item')
--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,7 @@
 from market import app ,db
 from flask import render_template ,redirect ,url_for ,flash 
 from market.models import Item ,User 
-from market.forms import RegisterForm, LoginForm
+from market.forms import RegisterForm, LoginForm ,PurchaseItemForm
 from flask_login import login_user ,logout_user , login_required
 
 @app.route('/')
@@ -12,8 +12,9 @@ def index():
 @app.route('/market')
 @login_required
 def market_page():
+    purchase = PurchaseItemForm()
     items = Item.query.all()
-    return render_template('market.html', items=items)
+    return render_template('market.html', items=items , purchase=purchase)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register_page():
